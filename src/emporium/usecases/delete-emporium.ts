ef04b78e259179c9285e68ec5e7972da40c4e4e8
dudubouneto <eduardobouhid.neto@gmail.com>
2023-10-24 22:24:01 -0300

    A first implementation of the Delete Endpoint Usecase.

--- src/emporium/usecases/delete-emporium.ts
+++ src/emporium/usecases/delete-emporium.ts
@@ -1,12 +1,33 @@
-import { Injectable } from '@nestjs/common'
-
-import MethodNotImplementedException from '@app/shared/exception/method-not-implemented-exception.exception'
-import { DeleteEmporiumInput } from '@app/emporium/input/delete-emporium.input'
-import { DeleteEmporiumOutput } from '@app/emporium/output/delete-emporium.output'
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { DeleteEmporiumInput } from '@app/emporium/input/delete-emporium.input';
+import { EmporiumEntity } from '@app/emporium/entities/emporium.entity';
+import { DeleteEmporiumOutput } from '@app/emporium/output/delete-emporium.output';
 
 @Injectable()
 export class DeleteEmporium {
-  handle(input: DeleteEmporiumInput): Promise<DeleteEmporiumOutput> {
-    throw new MethodNotImplementedException(this.handle.name)
+  constructor(
+    @InjectRepository(EmporiumEntity)
+    private readonly emporiumRepository: Repository<EmporiumEntity>,
+    @InjectRepository(DeleteEmporiumOutput)
+    private readonly deleteEmporiumOutputRepository: Repository<DeleteEmporiumOutput>,
+  ) {}
+
+  async handle(input: DeleteEmporiumInput): Promise<DeleteEmporiumOutput> {
+    const emporium = await this.emporiumRepository.findOne(input.id);
+    if (!emporium) {
+      throw new NotFoundException('Emporium not found');
+    }
+
+    emporium.isDeleted = true;
+    emporium.deletedAt = new Date();
+    await this.emporiumRepository.save(emporium);
+
+    // Create a DeleteEmporiumOutput record in the database
+    const deleteOutput = new DeleteEmporiumOutput();
+    await this.deleteEmporiumOutputRepository.save(deleteOutput);
+
+    return deleteOutput;
   }
 }
