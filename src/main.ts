c4fd4d380f90af9953ef33ab6844c90dee7e348d
themsiqueira <mateussiqueiracarneiro@outlook.com>
2023-11-27 20:39:19 -0300

    fix: update cors

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,14 @@ import { AppModule } from '@app/app.module'
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule)
+  app.enableCors()
+
   useContainer(app.select(AppModule), { fallbackOnErrors: true })
 
   const config = new DocumentBuilder().setTitle('Right Price API').setDescription('SoftwareEnginner').setVersion('1.0.0').build()
 
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('docs', app, document)
-
   await app.listen(3000)
 }
 
